e5560dbb5063a8b80684fea83c1b22681c7a5a7d
21ruedelyon <jerome.michel.serre@gmail.com>
2023-07-10 18:07:13 +0200

    Create Test.java

---
+++ Test/src/main/java/acme/Test.java
@@ -0,0 +1,12 @@
+package acme;
+
+import acme.util.Connexion;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Connexion c = new Connexion();
+		System.out.println(c);
+	}
+
+}
